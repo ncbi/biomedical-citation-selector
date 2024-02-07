16966ffeef4f94af6e4571248b0e7d677c1b44e4
raear <raear@nih.gov>
2024-02-07 16:30:22 -0500

    Update to Python 3.11 and scikit-learn 1.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 setuptools.setup(
         name="BmCS",
-        version="4.0.1",
+        version="4.0.2",
         author="Max Savery, Alastair Rae",
         author_email="alastair.rae@nih.gov",
         description="Biomedical Citation Selector for classification of MEDLINE citations",
@@ -19,18 +19,18 @@ setuptools.setup(
                 "BmCS=BmCS.BmCS:main"
                 ]},
         classifiers=[
-            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.11",
             "Operating System :: Unix"
             ],
         install_requires=[
-            "scikit-learn==0.24.1",
-            "tensorflow==2.8.4",
+            "scikit-learn==1.1.3",
+            "tensorflow==2.15.0.post1",
             "python-dateutil==2.8.1",
             "nltk==3.6.1",
-            "h5py==3.1.0",
-            "numpy==1.24.2",
-            "six==1.15.0",
-            "protobuf==3.19.6",
+            "h5py==3.10.0",
+            "numpy==1.26.4",
+            "six==1.16.0",
+            "protobuf==4.23.4",
             ],
         package_data={
             'BmCS': [
